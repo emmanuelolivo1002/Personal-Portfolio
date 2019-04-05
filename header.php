71cfd9500c94a8259f4ec518027702fe88fb1f93
Emmanuel Olivo <emmanuelo1002@gmail.com>
2019-04-05 09:33:48 -0500

    Add Fonts

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<?php wp_head(); ?>
+		<link href="https://fonts.googleapis.com/css?family=Inconsolata|Montserrat:500" rel="stylesheet">
 	</head>
 	<body <?php body_class(); ?>>
 
