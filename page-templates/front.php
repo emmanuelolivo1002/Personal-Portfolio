44c3b88806f8bdf178a5ea40c6b72ea2531eb127
Emmanuel Olivo <emmanuelo1002@gmail.com>
2019-04-11 08:40:28 -0500

    Refactor timeline to use css grid

--- page-templates/front.php
+++ page-templates/front.php
@@ -22,64 +22,62 @@ get_header(); ?>
 
 <section id="experience">
 	<h2>&lt;Experience&gt;</h2>
-	<div class="timeline" >
-		<div class="grid-container">
-			<div class="grid-x grid-margin-x">
-				<div class="cell small-12 medium-3"><span class="timeline__year">2016</span></div>
-				<div class="cell small-12 medium-3"><span class="timeline__year">2017</span></div>
-				<div class="cell small-12 medium-3"><span class="timeline__year">2018</span></div>
-				<div class="cell small-12 medium-3"><span class="timeline__year">2019</span></div>
-				<div class="timeline__divider"></div>
-				<div class="cell small-12 medium-3">
-					<div class="timeline__title">
-						<h3>TechCtrl</h3>
-					</div>
-					<div class="timeline__content">
-						<span class="timeline__content__position">Lead Developer & Computer Technician</span>
-						<ul class="timeline__content__list">
-							<li>Developed websites for multiple local clients. </li>
-							<li>Utilized knowledge of the repair process in order to provide customer feedback.</li>
-						</ul>
-					</div>
-				</div>
-				<div class="cell small-12 medium-3">
-					<div class="timeline__title">
-						<h3>Valley Technical Academy</h3>
-					</div>
-					<div class="timeline__content">
-						<span class="timeline__content__position">Website Developer and Teacher Assistant/Tutor</span>
-						<ul class="timeline__content__list">
-							<li>Assisted and tutored students enrolled in a Full Stack Web Development and User Experience Design course.</li>
-							<li>Taught a series of workshops in Web Development and iOS development.</li>
-						</ul>
-						<div class="timeline__title">
-							<h3>Cultura Agency</h3>
-						</div>
-						<div class="timeline__content">
-							<span class="timeline__content__position">Lead Developer</span>
-							<ul class="timeline__content__list">
-								<li>Developed websites for multiple local clients.</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<div class="cell small-12 medium-3">
-					<div class="timeline__title">
-						<h3>Imagine It Studios</h3>
-					</div>
-					<div class="timeline__content">
-						<span class="timeline__content__position">Developer/Tech Department</span>
-						<ul class="timeline__content__list">
-							<li>Developed websites for multiple clients that ranged from blog sites to e-commerce platforms.</li>
-							<li>Provided Search Engine Optimization and maintenance to approximately 20 clients’ websites.</li>
-						</ul>
-					</div>
-				</div>
-				<div class="cell small-12 medium-3">
-					<div class="timeline__title">
-						<h3>???</h3>
-					</div>
-				</div>
+	<div class="timeline " >
+		<div class="Year y2016"><span class="timeline__year">2016</span></div>
+		<div class="Year y2017"><span class="timeline__year">2017</span></div>
+		<div class="Year y2018"><span class="timeline__year">2018</span></div>
+		<div class="Year y2019"><span class="timeline__year">2019</span></div>
+		<div class="Divider timeline__divider"></div>
+		<div class="TechCtrl">
+			<div class="timeline__title">
+				<h3>TechCtrl</h3>
+			</div>
+			<div class="timeline__content">
+				<span class="timeline__content__position">Lead Developer & Computer Technician</span>
+				<ul class="timeline__content__list">
+					<li>Developed websites for multiple local clients. </li>
+					<li>Utilized knowledge of the repair process in order to provide customer feedback.</li>
+				</ul>
+			</div>
+		</div>
+		<div class="ValleyTech">
+			<div class="timeline__title">
+				<h3>Valley Technical Academy</h3>
+			</div>
+			<div class="timeline__content">
+				<span class="timeline__content__position">Website Developer and Teacher Assistant/Tutor</span>
+				<ul class="timeline__content__list">
+					<li>Assisted and tutored students enrolled in a Full Stack Web Development and User Experience Design course.</li>
+					<li>Taught a series of workshops in Web Development and iOS development.</li>
+				</ul>
+			</div>
+		</div>
+		<div class="Cultura">
+			<div class="timeline__title">
+				<h3>Cultura Agency</h3>
+			</div>
+			<div class="timeline__content">
+				<span class="timeline__content__position">Lead Developer</span>
+				<ul class="timeline__content__list">
+					<li>Developed websites for multiple local clients.</li>
+				</ul>
+			</div>
+		</div>
+		<div class="IIS">
+			<div class="timeline__title">
+				<h3>Imagine It Studios</h3>
+			</div>
+			<div class="timeline__content">
+				<span class="timeline__content__position">Developer/Tech Department</span>
+				<ul class="timeline__content__list">
+					<li>Developed websites for multiple clients that ranged from blog sites to e-commerce platforms.</li>
+					<li>Provided Search Engine Optimization and maintenance to approximately 20 clients’ websites.</li>
+				</ul>
+			</div>
+		</div>
+		<div class="Unknown">
+			<div class="timeline__title">
+				<h3>???</h3>
 			</div>
 		</div>
 	</div>
