9a3e6dcfe8b5d9404efb2abd6cf0e59a9d9ad0ac
Emmanuel Olivo <emmanuelo1002@gmail.com>
2019-04-12 09:56:43 -0500

    Add Fontawesome

--- header.php
+++ header.php
@@ -16,6 +16,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<?php wp_head(); ?>
 		<link href="https://fonts.googleapis.com/css?family=Inconsolata|Montserrat:500,600" rel="stylesheet">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 	</head>
 	<body <?php body_class(); ?>>
 
