ce66660caf04d2bfde42f7c6110c04c566bf3c5e
Emmanuel Olivo <emmanuelo1002@gmail.com>
2019-04-15 08:56:43 -0500

    Add Contact Section

--- page-templates/front.php
+++ page-templates/front.php
@@ -166,7 +166,36 @@ get_header(); ?>
 
 <section id="contact">
 	<h2>Contact Me</h2>
-	
+	<div class="grid-container">
+		<div class="grid-x grid-margin-x grid-margin-y">
+			<div class="cell small-12 large-6">
+				<div class="contact-form-container">
+					<?php echo do_shortcode('[wpforms id="8" title="false" description="false"]') ?>
+				</div>
+			</div>
+			<div class="cell small-12 large-6 info-container">
+				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio facilis eos ipsa quibusdam temporibus. Asperiores.</p>
+				<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio facilis eos ipsa quibusdam temporibus. Asperiores.</p>
+				<h3>Follow Me!</h3>
+				<div class="links-container">
+					<ul>
+						<li class="button-link">
+							<a href="#"><i class="fab fa-github"></i></a>
+						</li>
+						<li class="button-link">
+							<a href="#"><i class="fab fa-instagram"></i></a>
+						</li>
+						<li class="button-link">
+							<a href="#"><i class="fab fa-twitter"></i></a>
+						</li>
+						<li class="button-link">
+							<a href="#"><i class="fab fa-linkedin-in"></i></a>
+						</li>
+					</ul>
+				</div>
+			</div>			
+		</div>
+	</div>
 </section>
 
 
